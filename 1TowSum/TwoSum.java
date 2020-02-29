e3b976989177d27cfe181c37525fb118887e77d1
jiofeng <1255947967@qq.com>
2020-02-29 15:11:20 +0800

    twoSum

---
+++ 1TowSum/TwoSum.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author jiofeng
+ * @date 2020/2/29
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+
+    }
+}
+
+class TwoSumSolution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length - 1; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
+
+class OtherSolution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
