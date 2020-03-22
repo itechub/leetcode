8e2282458c798f5f947610d054b47b7af6a04189
jiofeng <jiofeng@qq.com>
2020-03-23 00:53:07 +0800

    146LRU

---
+++ 146LRU/LRUCacheSolution.java
@@ -0,0 +1,45 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author jiofeng
+ * @date 2020/3/23
+ */
+public class LRUCacheSolution {
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(2);
+
+        cache.put(1, 1);
+        cache.put(2, 2);
+        System.out.println(cache.get(1));
+        cache.put(3, 3);
+        System.out.println(cache.get(2));
+        cache.put(4, 4);
+        System.out.println(cache.get(1));
+        System.out.println(cache.get(3));
+        System.out.println(cache.get(4));
+    }
+}
+
+class LRUCache extends LinkedHashMap<Integer, Integer> {
+
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        super(capacity, 0.75f, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return super.size() > capacity;
+    }
+}
